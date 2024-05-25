d1a879bedabd4d985639281cbbac7cc2aa71287e
Dushmantha Ranathunga <e102.mavicsoft@gmail.com>
2024-05-25 12:47:56 +0530

    add: add deployed url

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000'; 
+const BASE_URL = 'https://invoice-384t.onrender.com'; 
 
 export const fetchInvoices = () => {
   return axios.get(`${BASE_URL}/invoices`);
